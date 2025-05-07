7c27a041ef44e5b6564d3934d3a22d215cd8202e
anasnide <anasnidirproject@gmail.com>
2025-05-07 17:57:21 +0800

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Welcome to MANEX</h2>
+      <p>This is the homepage.</p>
+    </div>
+  );
+};
+
+export default Home;
