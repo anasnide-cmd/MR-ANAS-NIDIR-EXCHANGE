3404b35b339962eb43d270f9c8bf4c274fe3309f
anasnide <anasnidirproject@gmail.com>
2025-05-07 15:58:00 +0800

    Update App.jsx

--- src /App.jsx
+++ src /App.jsx
@@ -1,1 +1,19 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import User from './User';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
