604314b8385d8690eb0db6b48cbffba2d51378b6
anasnide <anasnidirproject@gmail.com>
2025-05-07 18:00:03 +0800

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// Replace with your actual config
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AlzaSyC9G6ZGVtmo6zR8iZZZ5IldM5xOkuxLrG0",
+  authDomain: "manex-eb367.firebaseapp.com",
+  projectId: "manex-eb367",
+  storageBucket: "manex-eb367.appspot.com",
+  messagingSenderId: "340852859801",
+  appId: "1:340852859801:web:84bdd3da5a93d9fd432908"
+};
+
+export const app = initializeApp(firebaseConfig);
